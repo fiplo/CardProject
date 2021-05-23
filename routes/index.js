ec314345137987c0792ca187b6127191a08ee4e4
dalvis <dalius.visokinskas@ktu.edu>
2021-05-23 17:13:13 +0300

    Implemented user roles

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,57 @@
 var mongoose = require("mongoose");
 var User = require("../models/User");
 
-module.exports = function (app, passport, multer) {
-  app.get("/", function (req, res, next) {
-    res.render("index.ejs");
-  });
-
-  app.get("/login", function (req, res) {
-    res.render("login.ejs", { message: req.flash("loginMessage") });
-  });
-
-  app.post(
-    "/login",
-    passport.authenticate("local-login", {
-      successRedirect: "/profile",
-      failureRedirect: "/login",
-      failureFlash: true,
-    })
-  );
-
-  app.get("/signup", function (req, res) {
-    res.render("signup.ejs", { message: req.flash("signupMessage") });
-  });
-
-  app.post(
-    "/signup",
-    passport.authenticate("local-signup", {
-      successRedirect: "/profile",
-      failureRedirect: "/signup",
-      failureFlash: true,
-    })
-  );
-
-  app.get("/profile", isLoggedIn, function (req, res) {
-    res.render("profile.ejs", {
-      user: req.user,
+module.exports = function(app, passport, multer) {
+    app.get("/", function(req, res, next) {
+        if (req.user) {
+            res.render("index.ejs", { data: req.user.local });
+        } else {
+            res.render("index.ejs", { data: NaN });
+        }
     });
-  });
 
-  app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
-  });
+    app.get("/login", function(req, res) {
+        res.render("login.ejs", { message: req.flash("loginMessage") });
+    });
 
-  app.get("/users", function (req, res) {});
+    app.post(
+        "/login",
+        passport.authenticate("local-login", {
+            successRedirect: "/",
+            failureRedirect: "/login",
+            failureFlash: true,
+        })
+    );
 
-  function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
+    app.get("/signup", function(req, res) {
+        res.render("signup.ejs", { message: req.flash("signupMessage") });
+    });
 
-    res.redirect("/");
-  }
+    app.post(
+        "/signup",
+        passport.authenticate("local-signup", {
+            successRedirect: "/",
+            failureRedirect: "/signup",
+            failureFlash: true,
+        })
+    );
+
+    app.get("/profile", isLoggedIn, function(req, res) {
+        res.render("profile.ejs", {
+            user: req.user,
+        });
+    });
+
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
+    });
+
+    app.get("/users", function(req, res) {});
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) return next();
+
+        res.redirect("/");
+    }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,54 +1,58 @@
 var mongoose = require("mongoose");
 var User = require("../models/User");
 
-module.exports = function (app, passport) {
-  // Main Page
-  app.get("/", function (req, res) {
-    res.render("index.ejs");
-  });
-
-  //Login Screen
-  app.get("/login", function (req, res) {
-    res.render("login.ejs", { message: req.flash("loginMessage") });
-  });
-
-  //Processing login form
-  app.post(
-    "/login",
-    passport.authenticate("local-login", {
-      successRedirect: "/profile",
-      failureRedirect: "/login",
-      failureFlash: True,
-    })
-  );
-
-  //Signup
-  app.get("/signup", function (req, res) {
-    res.render("signup.ejs", { message: req.flash("singupMessage") });
-  });
-
-  //Process the signup form
-  app.post(
-    "/signup",
-    passport.authenticate("local-signup", {
-      successRedirect: "/profile",
-      failureRedirect: "/signup",
-      failureFlash: true,
-    })
-  );
-
-  //Profile page
-  app.get("/profile", isLoggedIn, function (req, res) {
-    res.render("profile.ejs", {
-      user: req.user,
+module.exports = function(app, passport) {
+    // Main Page
+    app.get("/", function(req, res) {
+        if (req.user) {
+            res.render("index.ejs", { data: req.user.local });
+        } else {
+            res.render("index.ejs", { data: NaN });
+        }
     });
-  });
 
-  app.get("/cards", isLoggedIn, function (req, res) {});
+    //Login Screen
+    app.get("/login", function(req, res) {
+        res.render("login.ejs", { message: req.flash("loginMessage") });
+    });
 
-  function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
+    //Processing login form
+    app.post(
+        "/login",
+        passport.authenticate("local-login", {
+            successRedirect: "/",
+            failureRedirect: "/login",
+            failureFlash: True,
+        })
+    );
 
-    res.redirect("/");
-  }
+    //Signup
+    app.get("/signup", function(req, res) {
+        res.render("signup.ejs", { message: req.flash("singupMessage") });
+    });
+
+    //Process the signup form
+    app.post(
+        "/signup",
+        passport.authenticate("local-signup", {
+            successRedirect: "/profile",
+            failureRedirect: "/signup",
+            failureFlash: true,
+        })
+    );
+
+    //Profile page
+    app.get("/profile", isLoggedIn, function(req, res) {
+        res.render("profile.ejs", {
+            user: req.user,
+        });
+    });
+
+    app.get("/cards", isLoggedIn, function(req, res) {});
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) return next();
+
+        res.redirect("/");
+    }
 };
