e884ed6a92dafb0370a58214537f4ea38ff3379d
paurat <paulius.ratkevicius@ktu.lt>
2021-05-24 05:56:12 +0300

    Updated /cards, should be full functionality

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,10 @@
 var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
-    card: [{ type: mongoose.Schema.Types.ObjectId, ref: "Card" }],
-    text: String,
-    created_at: Date,
+  card: [{ type: mongoose.Schema.Types.ObjectId, ref: "Card" }],
+  commenttext: String,
+  created_at: Date,
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
+
--- routes/cards.js
+++ routes/cards.js
@@ -5,9 +5,12 @@ var card = require("../controllers/CardController.js");
 
 //Gaunamas vardotojo kortu sarasas
 router.get("/", card.list);
+router.get("/show/:id", card.show);
 router.post("/save", card.save);
-router.get("/create",card.create);
-router.get("/remove/:id",card.remove)
+router.get("/create", card.create);
+router.get("/remove/:id", card.remove);
 router.get("/edit/:id", card.edit);
 router.post("/update/:id", card.update);
+router.get("/comment/:id", card.postComment);
+router.post("/comment/:id", card.insertCardComment);
 module.exports = router;
